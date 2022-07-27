010a78a71810bf152b95166ea8544fe75fe50b1a
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-07-27 08:26:06 -0700

    Created a student model to track in the database

---
+++ EntityFrameworkCore/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore
+{
+    public class Student
+    {
+        [Key] // Tell EF core to make this a primary key (PK)
+        public int Id { get; set; } 
+
+        public string FullName { get; set; } // object
+
+        public DateTime DateOfBirth { get; set; } // object
+    }
+}
--- EntityFrameworkCore/StudentContext.cs
+++ EntityFrameworkCore/StudentContext.cs
@@ -7,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCore
 {
-    internal class StudentContext : DbContext
+    public class StudentContext : DbContext
     {
+        public StudentContext() // constructor
+        {
+
+        }
+
+        // protected override was found here: https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli#create-the-model
+        protected override void OnConfiguring(DbContextOptionsBuilder options) // connects to SQLServer
+        {
+            // EF Connection Strings found here: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-strings#aspnet-core
+            // Server = The server we are connecting to. LocalDB is included with VS
+            // Database = The desired name for the database
+            // Trusted_Connection indicates that our windows account should be used 
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EntityFrameworkCoreDB;Trusted_Connection=True;");
+        }
     }
 }
